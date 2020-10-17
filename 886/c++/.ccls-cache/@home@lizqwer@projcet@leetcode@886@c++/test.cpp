c9c0b000109bfaaa8b14f5ee565321e59647398c
lizqwerscott <lizqwerscott@163.com>
2020-10-17 22:46:04 +0800

    first

---
+++ 886/c++/.ccls-cache/@home@lizqwer@projcet@leetcode@886@c++/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "stdio.h"
+#include <cmath>
+
+int main(int argc, char *argv[])
+{
+  std::cout << (int)((3 / 2) + 1) << std::endl; return 0;
+}
+
---
+++ 886/c++/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <sstream>
+#include "stdio.h"
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+int strToInt(const std::string & string_temp) {
+  int int_temp = 0;
+  std::stringstream stream(string_temp);
+  stream >> int_temp;
+  return int_temp;
+}
+
+int numberDigits(int number);
+
+std::vector<int> createPalindrom(int digit);
+
+std::vector<int> findPalindrome(int start);
+
+int primePalindrome(int N);
+
+int main(int argc, char *argv[])
+{
+  std::vector<int> temp  = findPalindrome(10);
+  for (int i = 0; i < temp.size(); i++) {
+    printf("Index:%d\n", temp[i]);
+  }
+  printf("result:%d\n", numberDigits(100)); 
+  printf("re:%d\n", numberDigits(10)); 
+  return 0;
+}
+
+int numberDigits(int number) {
+  std::string str = std::to_string(number);
+  return str.size();
+}
+
+std::vector<int> createPalindrom(int digit) {
+  std::vector<int> result;
+  int creatN = digit / 2; 
+  std::string endstr = "";
+  for (int i = 0; i < creatN; i++) {
+    endstr.append("9");
+  }
+  for (int i = pow(10, creatN - 1); i <= strToInt(endstr); i++) {
+    if (digit % 2 == 0) {
+      std::string z = std::to_string(i);
+      std::string rz = z;
+      reverse(rz.begin(), rz.end());
+      result.push_back(strToInt(z + rz));
+    } else {
+      for (int j = 1; j <= 9; j++) {
+        std::string z = std::to_string(i);
+        std::string rz = z;
+        reverse(rz.begin(), z.begin());
+        std::string zj = std::to_string(j);
+        result.push_back(strToInt(z + zj + rz));
+      }
+    }
+  }
+  return result;
+}
+
+std::vector<int> findPalindrome(int start) {
+  int digit = numberDigits(start);
+  return createPalindrom(digit);
+}
+
+
+int primePalindrome(int N) {
+  int result = N;
+  //1 find the Palindrome number; then in Palindrome number list find the prime.
+
+  
+
+  return result;
+}
